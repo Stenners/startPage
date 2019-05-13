d4d469591aaaefbfc6049ef450fa2001f4d20cf7
anthony.stenmark <anthony.stenmark@accenture.com>
2019-05-13 15:34:42 +1000

    Same tab search

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 // Search on enter key event
 function search_visibility(e) {
-    if (e.keyCode == 27 && (document.getElementById("search").style.display != "" || document.getElementById("search").style.display != "none")) {
-        document.getElementById("search").style.display = "";
-    }
-    if (document.getElementById("search").style.display != "flex") {
-        document.getElementById("search").style.display = "flex";
-        document.getElementById("search-field").select();
-    }
+  if (
+    e.keyCode == 27 &&
+    (document.getElementById("search").style.display != "" ||
+      document.getElementById("search").style.display != "none")
+  ) {
+    document.getElementById("search").style.display = "";
+  }
+  if (document.getElementById("search").style.display != "flex") {
+    document.getElementById("search").style.display = "flex";
+    document.getElementById("search-field").select();
+  }
 }
 
 function search(e) {
-    if (e.keyCode == 13) {
-        var val = document.getElementById("search-field").value;
-        window.open("https://google.com/search?q=" + val);
-    }
+  if (e.keyCode == 13) {
+    var val = document.getElementById("search-field").value;
+    // window.open("https://google.com/search?q=" + val);
+    window.location = `https://google.com/search?q=${val}`;
+  }
 }
-
